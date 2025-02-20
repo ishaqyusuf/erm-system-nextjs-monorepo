8d905ab4408b79a717659b7fd47a6e8a1e47b78c
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-20 14:49:27 +0100

    :tada:

---
+++ apps/dashboard/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+import baseConfig from "@gnd/ui/tailwind.config";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  presets: [baseConfig],
+  plugins: [],
+} satisfies Config;
