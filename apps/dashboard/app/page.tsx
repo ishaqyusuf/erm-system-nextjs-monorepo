575f47628712d962d633bddceba06afb2cc31337
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-20 15:24:39 +0100

    :tada:

--- apps/dashboard/app/page.tsx
+++ apps/dashboard/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import { db } from "@gnd/db";
 export default async function Home() {
   const user = await db.users.findFirst({});
-  return <div className="">{user.id}</div>;
+  return <div className="">{user?.id}</div>;
 }
