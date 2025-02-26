1e611c68b024b2baf542f13f2557ed799e3581ed
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-02-26 09:11:41 +0100

    :tada:

---
+++ apps/site/app/(clean-code)/(sales)/layout.tsx
@@ -0,0 +1,21 @@
+import UserAccountUpdateRequiredModal from "@/components/modals/user-account-update-required-modal";
+import { ContentLayout } from "../../../components/(clean-code)/content-layout";
+import SidebarLayout from "../../../components/(clean-code)/side-bar-layout";
+import BackwardCompat from "./_backward-compat";
+import { CustomerOverviewSheet } from "@/components/sheets/customer-overview-sheet";
+import { SalesPreviewModal } from "@/components/modals/sales-preview-modal";
+// import SalesOverviewSheet from "@/components/sheets/sales-overview-sheet";
+
+export default function Layout({ children }) {
+    return (
+        <SidebarLayout>
+            <ContentLayout>
+                <BackwardCompat />
+                {children}
+            </ContentLayout>
+            <CustomerOverviewSheet />
+            <SalesPreviewModal />
+            {/* <SalesOverviewSheet /> */}
+        </SidebarLayout>
+    );
+}
